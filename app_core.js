0d025a98180ba4b3543e887e9c469bdeaa931ae1
diegosportsfiorito-cmyk <diego.sports.fiorito@gmail.com>
2026-02-10 09:13:01 -0300

    Update app_core.js

--- app_core.js
+++ app_core.js
@@ -6,7 +6,7 @@ const AppCore = {
   config: {
     backendUrl:
       localStorage.getItem("backendUrl") ||
-      "https://stock-backend-1-0upi.onrender.com",
+      "https://stock-backend-1-0upi.onrender.com/query",
     modoDefecto: localStorage.getItem("modoDefecto") || "simple",
   },
 
